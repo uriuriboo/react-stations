696c25fc41eb3c17c16b7760e5584cc4c6ca4dbc
uriuriboo <rokkouribou@gmail.com>
2024-03-03 12:54:24 +0900

    station5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
+
+  const displayDog = (e) => {
+    setDogUrl("https://images.dog.ceo/breeds/entlebucher/n02108000_824.jpg")
+    // console.log(dogUrl["message"])
+    // console.log(dogUrl)
+  }
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogアプリ</header>
+      <img src={dogUrl} />
+      <form onClick={displayDog}>
+      <button type='button'>別の犬を表示</button>
+      </form>
     </div>
   )
 }
