623d4ac1eb384f66f95179f4360122c4bd4fbf0c
uriuriboo <rokkouribou@gmail.com>
2024-03-03 13:16:11 +0900

    station6

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ import { useState } from 'react'
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
 
-  const displayDog = (e) => {
-    setDogUrl("https://images.dog.ceo/breeds/entlebucher/n02108000_824.jpg")
-    // console.log(dogUrl["message"])
-    // console.log(dogUrl)
+  const displayDog = async (e) => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    const json_response = await response.json()
+    setDogUrl(json_response.message)
+    console.log(json_response.message)
   }
   return (
     <div>
