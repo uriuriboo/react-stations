9f4af4613c14033f353c26c5d6a1952e04bf4cff
uriuriboo <rokkouribou@gmail.com>
2024-03-05 18:16:46 +0900

    station10

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,24 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
+
+export const BreedsSelect = (props) => {
+  const {breeds, selectedBreed, setSelectedBreed} = props
+  const onChange = (e) => {
+    setSelectedBreed(e.target.value)
+  }
+  return (
+    <div>
+      <div className="dog_breeds_selecter">
+      <p>犬の種類を選択</p>
+      <select value={selectedBreed} onChange={onChange}>
+        {breeds.map(
+          (breed) => (
+          <option key={breed} value={breed}>{breed}</option>
+        ))}
+      </select>
+      </div>
+    </div>
+  )
 }
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,10 +1,12 @@
 // @ts-check
 
 import { useState, useEffect } from 'react'
+import BreedsSelect from './BreedsSelect'
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([]);
   const breeds_list_url = 'https://dog.ceo/api/breeds/list/all'
+  const [selectedBreed, setSelectedBreed] = useState('dog')
 
   useEffect(() => {
     fetch(breeds_list_url)
@@ -14,14 +16,20 @@ export const DogListContainer = () => {
         for (let i in dogs.message)
           array.push(i)
         setBreeds(array)
-        // console.log(array)
-        // console.log(breeds)
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       })
   }, [])
-  return <></>
+  return (
+    <div>
+      <BreedsSelect
+        breeds={breeds}
+        selectedBreed={selectedBreed}
+        setSelectedBreed={setSelectedBreed}
+        />
+    </div>
+  )
 }
 
 export default DogListContainer
